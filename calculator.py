4e3113105d2cc8397b8bbd325f6fd71d1a61d613
Vsingh1103 <136255451+Vsingh1103@users.noreply.github.com>
2025-11-18 10:21:54 +0530

    Improve calculator functionality and error handling
    Refactor calculator functions and add error handling.

--- calculator.py
+++ calculator.py
@@ -1,31 +1,42 @@
-def add(a, b):
-    return a + b
+def add(a,b):
+    return a+b
 
-def subtract(a, b):
-    return a - b
+def subtract(a,b):
+    return a-b
 
-def multiply(a, b):
-    return a * b
+def divide(a,b):
+    if b==0:
+        return "error:division by zero is not allowed."
+    return a/b
 
-def divide(a, b):
-    return a / b
+def multiply(a,b):
+    return a*b
+
+
 def main():
     print("simple calculator")
-    
-    
-    num1=int(input("Enter first number:"))
-    num2=int(input("Enter second number:"))
-    operation=input("Enter operation(+,-,*,/):")
-    if operation=='+':
-        print(add(num1,num2))
-    elif operation=='-':
-        print(subtract(num1,num2))
-    elif operation=='*':
-        print(multiply(num1,num2))
-    elif operation=='/':
-        print(divide(num1,num2))
-    else:
-        print("Invalid operation")
+    while True:
+        operator=input("Enter operator")
+        if operator.lower()=="exit":
+            print("exiting calculator.goodbye")
+            break
 
-if __name__ == "__main__":
+        try:
+            num1=int(input("Enter first number:"))
+            num2=int(input("Enter second number:"))
+        except valueError:
+            print("invalid input:please enter numeric values.")
+            continue
+        if operator=="+":
+                print(add(num1,num2))
+        elif operator=="-":
+                print(subtract(num1,num2))
+        elif operator=="*":
+                print(multiply(num1,num2))
+        elif operator=="/":
+                print(divide(num1,num2))
+        else:
+                print("invalid operator")
+
+if __name__=="__main__":
     main()
